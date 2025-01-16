ce7f0370b5c2b382653e0fc131feb66fb1af6084
Ellie Talamir <queenellivair@gmail.com>
2025-01-15 17:57:20 -0700

    Initial Repository Creation
    Created repository

---
+++ src/client/java/dev/queenellivair/plushies/ElliesVanillaPlushiesClient.java
@@ -0,0 +1,10 @@
+package dev.queenellivair.plushies;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ElliesVanillaPlushiesClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
